12f81dc5054b7a3d9ff264f37ec3ef432c1dcbfa
bmagario <brian.magario@gmail.com>
2023-08-23 10:24:26 -0300

    Queue

---
+++ src/test/java/com/bmagario/data_structures/queue/queue/Queue.java
@@ -0,0 +1,43 @@
+package com.bmagario.data_structures.queue.queue;
+
+public class Queue<T> {
+
+    private Node<T> front;
+    private Node<T> rear;
+
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data);
+
+        if (this.rear == null) {
+            this.front = newNode;
+            this.rear = newNode;
+        } else {
+            this.rear.setNext(newNode);
+            this.rear = newNode;
+        }
+    }
+
+    public T dequeue() {
+        if (this.front == null) {
+            return null;
+        }
+
+        T data = this.front.getData();
+        this.front = this.front.getNext();
+
+        if (this.front == null) {
+            this.rear = null;
+        }
+
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return this.front == null;
+    }
+}
---
+++ src/test/java/com/bmagario/data_structures/queue/queue/QueueTest.java
@@ -0,0 +1,50 @@
+package com.bmagario.data_structures.queue.queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.bmagario.models.JsonMockData;
+import com.bmagario.models.Sommelier;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class QueueTest {
+
+    @Test
+    public void testEnqueue() throws IOException {
+        Queue<Sommelier> sommelierQueue = new Queue<>();
+
+        List<Sommelier> sommeliersData = JsonMockData.getSommeliers();
+        sommeliersData.forEach(sommelierQueue::enqueue);
+
+        assertEquals(sommeliersData.get(0), sommelierQueue.dequeue());
+    }
+
+    @Test
+    public void testDequeu() throws IOException {
+        List<Sommelier> sommeliersData = JsonMockData.getSommeliers();
+        List<Sommelier> reversedSommeliers = new ArrayList<>(sommeliersData);
+        Collections.reverse(reversedSommeliers);
+
+        Queue<Sommelier> sommelierQueue = new Queue<>();
+        reversedSommeliers.forEach(sommelierQueue::enqueue);
+
+        assertEquals(sommeliersData.get(sommeliersData.size() - 1), sommelierQueue.dequeue());
+    }
+
+    @Test
+    public void testIsEmpty() throws IOException {
+        Queue<Sommelier> sommelierQueue = new Queue<>();
+
+        assertTrue(sommelierQueue.isEmpty());
+
+        List<Sommelier> sommeliersData = JsonMockData.getSommeliers();
+        sommeliersData.forEach(sommelierQueue::enqueue);
+
+        assertFalse(sommelierQueue.isEmpty());
+    }
+}
